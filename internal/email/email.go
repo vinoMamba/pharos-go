cb645f06a250859c6868a13a5871df913d707766
vino <vino0908@outlook.com>
2023-06-11 10:33:23 +0800

    使用viper环境变量

--- internal/email/email.go
+++ internal/email/email.go
@@ -1,6 +1,7 @@
 package email
 
 import (
+	"github.com/spf13/viper"
 	"gopkg.in/gomail.v2"
 )
 
@@ -11,7 +12,12 @@ func Send() {
 	m.SetHeader("Subject", "Hello!")
 	m.SetBody("text/html", "Hello <b>Bob</b> and <i>Cora</i>!")
 
-	d := gomail.NewDialer("0.0.0.0", 1025, "", "")
+	d := gomail.NewDialer(
+		viper.GetString("EMAIL_HOST"),
+		viper.GetInt("EMAIL_PORT"),
+		viper.GetString("EMAIL_USER"),
+		viper.GetString("EMAIL_PASSWD"),
+	)
 	if err := d.DialAndSend(m); err != nil {
 		panic(err)
 	}
